54c3267cd1b44c7b0ad1d974f816e305c6afc602
Adnan Sameer <adnansameer62@gmail.com>
2023-11-05 10:54:37 +0500

    feat: check if token exists for /users api

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,20 +1,28 @@
 import { connectMongoDb } from "@/lib/mongoDb";
 import { NextResponse } from "next/server";
 import { getUsers } from "@/services/usersService";
+import { getToken } from "next-auth/jwt";
+
 export async function GET(req) {
   try {
-    const url = new URL(req.url);
-    const page = url.searchParams.get("page") || 1;
-    const limit = url.searchParams.get("limit") || 2;
-    const search = url.searchParams.get("search") || "";
-    const sortBy = url.searchParams.get("sortBy") || "username";
-    await connectMongoDb();
-    const usersData = await getUsers(page, limit, search, sortBy);
-
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    if (token) {
+      const url = new URL(req.url);
+      const page = url.searchParams.get("page") || 1;
+      const limit = url.searchParams.get("limit") || 2;
+      const search = url.searchParams.get("search") || "";
+      const sortBy = url.searchParams.get("sortBy") || "username";
+      await connectMongoDb();
+      const usersData = await getUsers(page, limit, search, sortBy);
+      return NextResponse.json({
+        data: usersData,
+        message: "Users returned Succesfully",
+        status: 200,
+      });
+    }
     return NextResponse.json({
-      data: usersData,
-      message: "Users returned Succesfully",
-      status: 200,
+      message: "User Unauthorized",
+      status: 401,
     });
   } catch (err) {
     throw NextResponse.json({
