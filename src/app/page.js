853676007b5466629037e77ac90272c7016c0d34
Adnan Sameer <adnansameer62@gmail.com>
2023-11-05 11:10:09 +0500

    feat: remove unused console

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,6 @@ import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
-  console.log("🚀 ~ file: page.js:12 ~ Home ~ session:", session);
 
   if (session) redirect("/users");
   return (
